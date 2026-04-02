3710030bed369a71a3148da972c278e089a339a4
manavimukthi <manavimukthiinduja77@gmail.com>
2026-04-02 11:46:16 +0530

    fix: ignore eslint and ts errors

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,  // ← add this line
-  },
 }
 
 export default nextConfig
